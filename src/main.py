9f27018ec4772464e1b969e6db2ff2b7d2685725
ValterS <valters18@gmail.com>
2021-11-17 13:59:31 +0000

    starship pre-merge

--- src/main.py
+++ src/main.py
@@ -8,17 +8,67 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Starships, DetailsStarship
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+
+
+
+@app.route('/starships', methods=['GET'])
+def get_starships():
+    starships = Starships.get_all_starships()
+    all_starships = [starship.to_dict() for starship in starships]
+    return jsonify(all_starships), 200
+
+
+@app.route('/starships/<int:id>', methods=['GET'])
+def get_starship_by_id(id):
+    starship = Starships.get_starship_id(id)
+    return jsonify(starship.to_dict()), 200
+    return jsonify({'error': 'Starship not found'}), 400
+
+
+# @app.route('/starships', methods=['POST'])
+# def create_starship():
+#     new_starship = request.json.get('name', None)
+
+#     if not new_starship:
+#         return jsonify({'error' : 'Missing starship name'}), 400
+#     starship = Starships(name=new_starship, properties="")
+
+#     starship_created = starship.create_new_starship()
+#     return jsonify(starship_created.to_dict()), 201
+
+
+@app.route('/starships/<int:id>/details', methods=['GET'])
+def select_details(id):
+    starship = Starships.get_starship_id(id)
+    if starship:
+        details = DetailsStarship.get_id(starship.id_details)
+    if details:
+        return jsonify(details.to_dict()), 200 
+    
+    return jsonify({'error':'Details not found'}), 400
+
+  
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,105 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Login(db.Model):
+    __tablename__ = 'login'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(250), unique= True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    _is_active = db.Column(db.Boolean, nullable=False)
+    
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'id:{seld.id}, username: {self.username}'
 
-    def serialize(self):
+    
+    def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username, 
+        }
+    
+    @classmethod
+    def get_user_by_email(cls, email_user):
+        user = cls.query.filter_by(email = email_user).one_or_none()
+    
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+
+
+class DetailsStarship(db.Model):
+    __tablename__ = 'detailsStarship'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False) 
+    model = db.Column(db.String(250), nullable=False)
+    cost_in_credits = db.Column(db.FLOAT, nullable=False)
+    max_atmosphering_speed = db.Column(db.FLOAT, nullable=False)
+
+    starship = db.relationship("Starships", back_populates="details")
+
+    def __repr__(self):
+        return f'Starship name is: {self.name}, its manufactured by {self.manufacturer}, model {self.model}, it costs {self.cost} credits, and has a max atmospheric speed of {self.max_atmosphering_speed} km/h.'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "manufacturer": self.manufacturer,
+            "model" : self.model,
+            "cost_in_credits": self.cost_in_credits,
+            "max_atmosphering_speed": self.max_atmosphering_speed  
+        } 
+
+
+    @classmethod
+    def get_id(cls, id_details):
+        detail = cls.query.get(id_details)
+        return detail
+
+
+
+
+class Starships(db.Model):
+    __tablename__ = 'starships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    id_details = db.Column(db.Integer, db.ForeignKey("detailsStarship.id"), unique = True)
+
+    details = db.relationship("DetailsStarship", back_populates="starship")
+    
+
+    def __repr__(self):
+        return f'Starship name is: {self.name} and its properties are {self.properties}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "id_details": self.details.to_dict()
             # do not serialize the password, its a security breach
         }
+
+    @classmethod
+    def get_all_starships(cls):
+        all_starships = cls.query.all()
+        return all_starships
+    
+    @classmethod
+    def get_starship_id(cls, id):
+        starship = cls.query.filter_by(id=id).one_or_none()
+        return starship
+
+    def create_new_starship(self):
+        db.session.add(self)
+        db.session.commit()
+        return self 
+
+
+
+
